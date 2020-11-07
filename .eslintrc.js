95b5e47b39c447e760610025461b5c68ee184509
yanhuo <704021137@qq.com>
2020-11-07 16:30:47 +0800

    第一次提交项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    // '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    'prefer-const': 0,
+    'no-trailing-spaces': 0,
+    'no-multiple-empty-lines': 0,
+    'eol-last': 0,
+    indent: 0
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <router-view />
     <van-tabbar v-model="active">
-      <van-tabbar-item to='/'>书架</van-tabbar-item>
-      <van-tabbar-item to='/'>书城</van-tabbar-item>
-      <van-tabbar-item to='/'>我的</van-tabbar-item>
+      <van-tabbar-item to="/">书架</van-tabbar-item>
+      <van-tabbar-item to="/">书城</van-tabbar-item>
+      <van-tabbar-item to="/">我的</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
