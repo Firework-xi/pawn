0a7eb3f8fbb8b19faa171565051f743d6686cf3f
zhouqiaoqiao <1312094910@qq.com>
2020-11-08 21:38:24 +0800

    第三次登陆

--- src/components/my/login/login-choice.vue
+++ src/components/my/login/login-choice.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="login-choice">
+    <div class="skip"><span>随便看看>></span></div>
     <!-- 标题 -->
     <div class="login-Gender">选择性别</div>
     <!-- /标题 -->
     <!-- 男女选择 -->
     <div class="login-center">
       <div class="login-img">
-        <van-image width="80" round fit="cover" height="80" @click="nan" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <van-image width="80" round fit="cover" height="80" @click="male" :src="require('../../../assets/d99dce9d81c02014d698ce04a63d59f.png')" />
         <span class="gender">男生</span><span class="short-essay">热血爽文，酷帅狂拽</span>
       </div>
       <div class="login-img">
-        <van-image width="80" round fit="cover" @click="nv" height="80" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <van-image width="80" round fit="cover" height="80" @click="female" :src="require('../../../assets/e6149428815a8470ea55408fcc65d10.png')" />
         <span class="gender">女生</span><span class="short-essay">总裁王爷，绝不将就</span>
       </div>
     </div>
     <!-- /男女选择 -->
     <!-- 立即体验按钮 -->
     <div class="but">
-      <van-button class="login-button" size="large" round type="default" @click="onclick">立即体验</van-button>
+      <van-button class="login-button" size="large" color="#ffbd83" plain round @click="LoginExperience">立即体验</van-button>
     </div>
     <!-- /立即体验按钮 -->
   </div>
@@ -38,19 +39,52 @@
   created () {},
   mounted () {},
   methods: {
-    onclick() {
+    // 立即体验事件
+    LoginExperience() {
       if (this.index === null) {
-        console.log()
-      }
-      window.localStorage.setItem('xingbei', this.index)
-    }, 
-     nan() {
-      this.index = 1
-      // 调用网络请求，请求成功之后 保存在本地
-    }, 
-     nv() {
+        return this.$toast('请选择性别')
+      } 
+      this.$toast.loading({
+        message: '加载中...',
+         forbidClick: true
+        })
+      window.localStorage.setItem('choice', this.index)
+    },
+    male() {
       this.index = 0
+    },
+    female() {
+      this.index = 1
     }
+
+
+
+
+
+  //  async onclick() {
+  //   //  判断是否选择的男或女
+  //     if (this.index === null) {
+  //       this.$toast('请选择性别')
+  //     } else {
+  //       // 加载中
+  //       this.$toast.loading({
+  //       message: '加载中...',
+  //        forbidClick: true
+  //       })
+  //       window.localStorage.setItem('choice', this.index)
+  //         const { data } = await this.$http.get('http://yuedu/details')
+  //         console.log(data)
+  //     }
+  //   }, 
+  //   // 男生
+  //    male() {
+  //     this.index = 1
+  //     // 调用网络请求，请求成功之后 保存在本地
+  //   }, 
+  //   // 女生
+  //    female() {
+  //     this.index = 0
+  //   }
   }
     
   }
@@ -58,10 +92,30 @@
 
 <style lang="scss" scoped>
 .login-choice {
+  .van-image {
+    background-color: rgb(255, 238, 188);
+    // border: 6px solid #eee;
+  }
+  .skip {
+    margin: 20px;
+    margin-top: 30px;
+
+    font-size: 26px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    span {
+      color: rgb(149, 149, 149);
+      padding: 20px;
+    }
+  }
+  color: #333;
   flex-direction: column;
   margin-top: 140px;
   align-content: center;
+
   .login-Gender {
+    color: #333;
     text-align: center;
     font-size: 60px;
   }
@@ -87,8 +141,10 @@
       .gender {
         margin-top: 16px;
         font-size: 36px;
+        color: #ff9656;
       }
       .short-essay {
+        color: rgb(149, 149, 149);
         font-size: 24px;
       }
     }
--- src/components/my/login/login-land.vue
+++ src/components/my/login/login-land.vue
@@ -2,30 +2,65 @@
   <div class="login-land">
     <!-- 春暖阅读 -->
     <div class="login-logo">
-      <van-image width="90" round fit="cover" height="90" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+      <van-image width="90" round fit="cover" height="90" :src="require('../../../assets/logo (2).png')" />
       <span class="text1">春暖阅读</span>
       <span class="text2">面朝大海，春暖花开</span>
     </div>
     <!-- /春暖阅读 -->
+    <!-- 登陆-->
+    <van-form @submit="onSubmit" class="land">
+      <van-field v-model="username" name="user" placeholder="请输入用户名" :rules="userFormRules.user" maxlength="6"
+        ><template #left-icon><van-icon name="friends-o" /> </template>
+      </van-field>
+      <van-field v-model="password" type="password" name="密码" placeholder="请输入密码" :rules="userFormRules.password" maxlength="6"
+        ><template #left-icon><van-icon name="bag-o" /> </template
+      ></van-field>
+
+      <div style="margin: 20px 30px 10px">
+        <van-button round block color="#ffca4f" size="normal"> 登陆 </van-button>
+      </div>
+      <!-- 去注册 -->
+      <div class="register">去注册>></div>
+      <!-- /去注册 -->
+    </van-form>
+    <!-- /登陆 -->
+    <!-- 去注册 -->
+    <van-form @submit="onSubmit" class="land">
+      <van-field v-model="username" name="user2" placeholder="请输入手机号" :rules="userFormRules.user2" maxlength="11" type="number"
+        ><template #left-icon><van-icon name="friends-o" /> </template>
+      </van-field>
+      <van-field v-model="password" type="password" name="密码" placeholder="请输入密码" :rules="userFormRules.password" maxlength="6"
+        ><template #left-icon><van-icon name="bag-o" /> </template
+      ></van-field>
+
+      <div style="margin: 20px 30px 10px">
+        <van-button round block color="#ffca4f" size="normal"> 去登陆 </van-button>
+      </div>
+    </van-form>
+
+    <!-- /去注册 -->
     <!-- 一键登陆 -->
-    <div class="one-click-login"></div>
+    <van-divider @click="showShare = true" class="one-click-login" :style="{ color: '#999', borderColor: '#222', padding: '0 80px' }">使用第三方登陆 </van-divider>
     <!-- /一键登陆 -->
     <!-- 微信、qq、微博登陆 -->
-    <div class="three-party-landing">
+    <!-- <div class="three-party-landing">
       <div>
-        <van-image width="50" round fit="cover" height="50" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <van-image width="50" round fit="cover" height="50" :src="require('../../../assets/wx.png')" @click="showShare = true" />
         <span>微信登陆</span>
       </div>
       <div>
-        <van-image width="50" round fit="cover" height="50" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+        <van-image width="50" round fit="cover" height="50" :src="require('../../../assets/wb.png')" @click="showShare = true" />
         <span>微信登陆</span>
       </div>
       <div>
-        <van-image width="50" round fit="cover" height="50" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-        <span>微信登陆</span>
+        <van-image width="50" round fit="cover" height="50" :src="require('../../../assets/qq.png')" @click="showShare = true" />
+        <span>QQ登陆</span>
       </div>
-    </div>
+    </div> -->
     <!-- /微信、qq、微博登陆 -->
+    <!-- 弹框 -->
+    <van-share-sheet v-model="showShare" title="一键登陆" :options="options" @select="onSelect" />
+    <!-- /弹框 -->
   </div>
 </template>
 
@@ -36,33 +71,67 @@
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+       username: '',
+      password: '',
+       showShare: false,
+      // 第三方登陆
+      options: [
+        { name: '微信', icon: 'wechat' },
+        { name: '微博', icon: 'weibo' },
+         { name: 'QQ', icon: 'qq' }
+      ],
+      userFormRules: {
+        user: [{ required: true }],
+        user2: [{ required: true }],
+        password: [{ required: true }]
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+     onSelect(option) {
+       this.$toast.loading({
+       message: '加载中...',
+       forbidClick: true
+})
+      // this.$toast(option.name)
+      this.showShare = false
+    },
+     onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
   }
 </script>
 
 <style lang="scss" scoped>
 .login-land {
+  color: #333;
+  // .van-image {
+  //   border: 4px solid #eee;
+  // }
   .login-logo {
-    margin-top: 300px;
+    margin-top: 200px;
     display: flex;
     flex-direction: column;
     align-items: center;
     .text1 {
       font-size: 60px;
+      color: #333;
     }
     .text2 {
       font-size: 28px;
+      color: rgb(149, 149, 149);
     }
   }
   .three-party-landing {
     margin: 50px;
     display: flex;
+
     div {
       flex: 1;
       display: flex;
@@ -70,15 +139,28 @@
       align-items: center;
       span {
         font-size: 20px;
+        color: rgb(149, 149, 149);
       }
     }
   }
-  .one-click-login {
-    margin: 0 auto;
-    margin-top: 100px;
-    width: 600px;
-    height: 1px;
-    background-color: #eee;
+  // .one-click-login {
+  //   margin-top: 20px;
+  // }
+  ::v-deep .land {
+    padding: 50px;
+    .van-icon {
+      font-size: 60px;
+      // color: #ffca4f;
+    }
+    .register {
+      font-size: 26px;
+      text-align: center;
+      color: rgb(149, 149, 149);
+    }
+    .van-button {
+      font-size: 38px;
+      margin-bottom: 10px;
+    }
   }
 }
 </style>
