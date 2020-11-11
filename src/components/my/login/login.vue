ade2800836de3bfcf861fd6e479bbbe728321cf4
xiang <328466685@qq.com>
2020-11-11 21:03:53 +0800

    修改了代码

--- src/components/my/login/login.vue
+++ src/components/my/login/login.vue
@@ -1,34 +1,29 @@
 <template>
   <div>
     <!-- <van-image :class="isone ? '' : 'van'" @click="isone = false" width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" /> -->
-    <van-image  width="100" height="100" @click="isones" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-    <van-image  width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+    <van-image width="100" height="100" @click="isones" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+    <van-image width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
   </div>
 </template>
 
 <script>
-  export default {
-     name: 'name',
+export default {
+  name: 'name',
   components: {},
   props: {},
-  data () {
+  data() {
     return {
       isone: true
-      
-      
     }
   },
   computed: {},
   watch: {},
-  created () {
-    
-  },
-  mounted () {},
-  methods: { 
-    isones(){} 
+  created() {},
+  mounted() {},
+  methods: {
+    isones() {}
   }
-    
-  }
+}
 </script>
 
 <style lang="scss" scoped>
