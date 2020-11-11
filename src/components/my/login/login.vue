8f7179a1372b45a02472f669545df7740eb7e468
zhouqiaoqiao <1312094910@qq.com>
2020-11-11 15:12:45 +0800

    意外错误

--- src/components/my/login/login.vue
+++ src/components/my/login/login.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <!-- <van-image :class="isone ? '' : 'van'" @click="isone = false" width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" /> -->
-    <van-image  width="100" height="100" @click="isones" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-    <van-image  width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+    <van-image width="100" height="100" @click="isones" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+    <van-image width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
   </div>
 </template>
 
@@ -25,7 +25,7 @@
   },
   mounted () {},
   methods: { 
-    isones(){} 
+    isones() {} 
   }
     
   }
